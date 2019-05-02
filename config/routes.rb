846986dc2d22ddbcec6626e798d92835adbd66fa
rails course from udemy <lomaslove132@gmail.com>
2019-05-02 15:06:40 +1000

    added sign in and more

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'pages/contact'
   get 'orders/new'
   devise_for :users
-  get 'home/index'
+  #get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "listings#index"
   
