1f2b6ebaf4b79b20dc69600c4f79c5655e5a09bd
Ronnoci <cmcin39@gmail.com>
2019-05-07 11:18:07 +1000

    added views for orders new and create

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'pages/about'
   get 'pages/contact'
   get 'orders/new'
+  get 'success', to: "orders#create"
   devise_for :users
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
