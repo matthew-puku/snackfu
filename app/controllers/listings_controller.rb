501db996472f5cc7133fb1e8729b1ffb5f20e9cb
Ronnoci <cmcin39@gmail.com>
2019-05-10 11:06:30 +1000

    Implemented search bar

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -10,7 +10,15 @@ class ListingsController < ApplicationController
 
 
   def index
-    @listings = Listing.all.order("created_at DESC")
+   
+    search_term = params[:search]
+  
+      if search_term.blank?
+          @listings = Listing.all
+      else
+          @listings = Listing.where("upper(name) LIKE ?", "%#{search_term.upcase}%")
+      end
+      # @listings = Listing.all.order("created_at DESC")
   end
 
   # GET /listings/1
